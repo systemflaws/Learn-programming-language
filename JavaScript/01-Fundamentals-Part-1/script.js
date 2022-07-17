b904ca057db69b518b994db33c0853c6f9cd1d3e
Arun Kumar Singh <arun.krsingh1@outlook.com>
2022-07-17 17:59:52 +0530

    template literals if-else type conversion and coercioin

--- JavaScript/01-Fundamentals-Part-1/script.js
+++ JavaScript/01-Fundamentals-Part-1/script.js
@@ -86,3 +86,118 @@ console.log(arunAge >= rajuAge);
 
 console.log(currentYear - arunBirthYear >= currentYear - rajuBirthYear);
 */
+
+// Coding Challenge #1
+
+/*
+const marksMass = 95;
+const marksHeight = 1.88;
+const johnsMass = 85;
+const johnsHeight = 1.76;
+
+const marksBMI = (marksMass) / (marksHeight ** 2);
+const johnsBMI = (johnsMass) / (johnsHeight * johnsHeight);
+const markHigherBMI = (marksBMI > johnsBMI);
+
+console.log(marksBMI, johnsBMI);
+console.log(markHigherBMI);
+*/
+
+// Template literals
+/*
+const firstName = 'Arun';
+const birthYear = 1999;
+const job = 'unemployed';
+const currentYear = 2022;
+
+const about = "I'm " + firstName + ". I'm " + (currentYear-birthYear) + " year old " + job + "!";
+console.log(about);
+
+const aboutTemplateLiteral = `I'm ${firstName}. I'm ${currentYear - birthYear} year old ${job}!`;
+console.log(aboutTemplateLiteral);
+
+console.log(`I'm doing not well...`);
+
+console.log("This is \nmultiline \ncomment \n");
+console.log("This is \n\
+multiline\n\
+comment.")
+
+console.log(`This is 
+multiline
+comment`);
+*/
+
+// if-else statement
+
+/*
+const age = 15;
+// const legalAge = age >= 18;
+// if(true){statement;}
+
+if(age >= 18){
+    console.log(`You can drive 🚗 or drink.🍷`);
+}
+else{
+    console.log(`You can't drive and drink.`);
+    const yearsLeft = 18 - age;
+    console.log(`Wait ${yearsLeft} years.`);
+}
+
+const birthYear = 1998;
+let century;
+if(birthYear <= 2000){
+    century = 20;
+    console.log(century);
+}
+else{
+    century = 21;
+    console.log(century);
+}
+*/
+
+// Coding Challenge #2
+/*
+const marksMass = 95;
+const marksHeight = 1.88;
+const johnsMass = 85;
+const johnsHeight = 1.76;
+
+const marksBMI = (marksMass) / (marksHeight ** 2);
+const johnsBMI = (johnsMass) / (johnsHeight * johnsHeight);
+
+if(marksBMI > johnsBMI){
+    console.log(`Mark's BMI ${marksBMI.toFixed(2)} is higher than John's BMI ${johnsBMI.toFixed(2)}.`);
+}
+else{
+    console.log(`John's BMI ${johnsBMI.toFixed(2)} is higher than Mark's BMI ${marksBMI.toFixed(2)}.`);
+}
+*/
+
+/*
+// Type Conversion: Do explicitly
+const year = '1998';
+console.log(year, Number(year));
+
+console.log(year + 18); // Type Coercion
+console.log(Number(year) + 18);
+
+console.log(Number('arun'));
+
+console.log(typeof NaN);
+
+console.log(18);
+console.log(String(18));
+
+// Type Coercion: Do implicitly
+
+console.log("Im" + 18 + "year old");
+console.log("25" - "25" - 3);
+console.log("25" * 2);
+console.log(25 / "5");
+
+let number = '25' + 1;
+number--;
+console.log(number);
+// Can introduce bug
+*/
