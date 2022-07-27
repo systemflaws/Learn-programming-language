11985a057d10beac3a5d51e791b41874835800a9
Arun Kumar Singh <arun.krsingh1@outlook.com>
2022-07-27 21:57:29 +0530

    6 Type of sort programs add

--- Cpp/Algorithm/Sorting/all_sorts.cpp
+++ Cpp/Algorithm/Sorting/all_sorts.cpp
@@ -88,6 +88,85 @@ void countingSort(vector<int> &nums)
     }
 }
 
+void merge(vector<int> &numbers, int low, int mid, int high)
+{
+    int i = low, j = mid + 1, k = 0;
+    vector<int> temp(high - low + 1, 0);
+    while (i <= mid && j <= high)
+    {
+        if (numbers[i] <= numbers[j])
+        {
+            temp[k] = numbers[i];
+            i++;
+        }
+        else
+        {
+            temp[k] = numbers[j];
+            j++;
+        }
+        k++;
+    }
+    while (i <= mid)
+    {
+        temp[k++] = numbers[i++];
+    }
+    while (j <= high)
+    {
+        temp[k++] = numbers[j++];
+    }
+    for (int index = 0; index < temp.size(); index++)
+    {
+        numbers[low + index] = temp[index];
+    }
+    return;
+}
+
+void mergeSort(vector<int> &numbers, int low, int high)
+{
+    // base Case
+    if (low >= high)
+    {
+        return;
+    }
+    // recursive Case
+    int mid = low + (high - low) / 2;
+    mergeSort(numbers, low, mid);
+    mergeSort(numbers, mid + 1, high);
+    return merge(numbers, low, mid, high);
+}
+
+int qSort(vector<int> &numbers, int low, int high)
+{
+    int pivot = high; // last element
+    int i = low - 1; // initialize
+    int j = low; // Running pointer
+    while (j < pivot)
+    {
+        if (numbers[j] <= numbers[pivot]) // When element is less then pivot then swap with the i + 1 and j.
+        {
+            swap(numbers[++i], numbers[j]);
+        }
+        j++;
+    }
+    swap(numbers[++i], numbers[pivot]);// to place the pivot in the right position
+    return i; // returning the pivot element position
+}
+
+void quickSort(vector<int> &numbers, int low, int high) // 
+{
+    // base Case
+    if (low >= high)
+    {
+        return;
+    }
+    // recursive Case
+    int index = qSort(numbers, low, high); // find the index of the pivot to divide array
+    // cout << index << endl;
+    quickSort(numbers, low, index - 1); // recursively sort left part
+    quickSort(numbers, index + 1, high); // recursively sort right part
+    return;
+}
+
 int main()
 {
     cout << "Enter the number of elements: ";
@@ -101,7 +180,7 @@ int main()
     }
     cout << "Elements before sorting : ";
     printing(numbers);
-    cout << "1. Bubble sort\n2. Insertion sort \n3. Selection Sort \n4. Counting Sort" << endl;
+    cout << "1. Bubble sort\n2. Insertion sort \n3. Selection Sort \n4. Counting Sort \n5. Merge Sort \n6. Quick Sort" << endl;
     cout << "Enter the value for which algorithm you want to use: ";
     int value;
     cin >> value;
@@ -119,6 +198,12 @@ int main()
         break;
     case 4:
         countingSort(numbers);
+        break;
+    case 5:
+        mergeSort(numbers, 0, numbers.size() - 1);
+        break;
+    case 6:
+        quickSort(numbers, 0, numbers.size() - 1);
         break;
     }
     cout << "Elements after sorting : ";
