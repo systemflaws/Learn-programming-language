537ecfb636441fd4ae38b9a8b6efa20a6438c14d
Arun Kumar Singh <arun.krsingh1@outlook.com>
2022-07-27 22:27:09 +0530

    information add

--- Cpp/Data-Structure/Vector/myVector.cpp
+++ Cpp/Data-Structure/Vector/myVector.cpp
@@ -2,17 +2,16 @@
 
 using namespace std;
 
-template <typename T>
+template <typename T> // Template creation
 class Vector
 {
-    int currentSize;
+    int currentSize; // 
     int maxSize;
-    T *ptr;
+    T *ptr; // pointer to the Type T memory
 
 public:
-    Vector(int ms = 1) : currentSize{0}, maxSize{ms}, ptr{new T[maxSize]}
-    {
-    }
+    Vector(int ms = 1) : currentSize{0}, maxSize{ms}, ptr{new T[maxSize]} // Intialisation List
+    {}
 
     void push_back(const T value)
     {
@@ -20,15 +19,16 @@ public:
         {
             T *oldPtr = ptr;
             maxSize = 2 * maxSize;
-            ptr = new T[maxSize];
+            ptr = new T[maxSize]; // bigger size array is created
             for (int i = 0; i < currentSize; i++)
             {
                 ptr[i] = oldPtr[i];
             }
-            delete[] oldPtr;
+            delete[] oldPtr; // To get ride of memory leak
         }
         ptr[currentSize++] = value;
     }
+
     void pop_back()
     {
         if (!empty())
@@ -38,6 +38,7 @@ public:
         }
         cout << "Already Empty" << endl;
     }
+
     bool empty() const
     {
         return currentSize == 0;
@@ -62,6 +63,7 @@ public:
         cout << "Empty ";
         return -1;
     }
+
     T at(const int index) const
     {
         if (index < 0 || index >= currentSize)
@@ -71,14 +73,17 @@ public:
         }
         return ptr[index];
     }
+
     int size() const
     {
         return currentSize;
     }
+
     int capacity() const
     {
         return maxSize;
     }
+
     T operator[](const int index) const
     {
         if (index < 0 || index >= currentSize)
@@ -88,6 +93,7 @@ public:
         }
         return ptr[index];
     }
+
     void printing() const
     {
         for (int i = 0; i < currentSize; i++)
@@ -96,20 +102,21 @@ public:
         }
         cout << endl;
     }
+
 };
 
 int main()
 {
     // Vector v1(1); Without Template
     Vector<int> v1(1);
-    cout << v1.size() << endl;
-    v1.push_back(10);
-    v1.push_back(11);
-    cout << v1.back() << endl;
-    cout << v1.front() << endl;
-    v1.push_back(12);
-    v1.printing();
-    v1.pop_back();
-    v1.printing();
-    cout << v1.at(1) << endl;
+    cout << v1.size() << endl; // cs=0, ms = 1
+    v1.push_back(10);  // 10
+    v1.push_back(11); // 10, 11
+    cout << v1.back() << endl; // 11
+    cout << v1.front() << endl; // 10
+    v1.push_back(12); // 10, 11, 12
+    v1.printing(); // 10, 11, 12
+    v1.pop_back(); // 10, 11
+    v1.printing(); // 10, 11
+    cout << v1.at(1) << endl; // 11
 }
